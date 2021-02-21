2a7f66a3b6782e275cc99dd0e9dd367758d5a714
ArekuDev <git@russelldzine.com>
2021-02-21 18:58:17 -0500

    Update index.test.php

--- index.test.php
+++ index.test.php
@@ -1,6 +1,5 @@
 <?php include "deck.class.php";
 
-
 $myCards = new DeckOfCards;
 
 //// TEST: Deal cards until deck is empty
@@ -10,10 +9,9 @@ $myCards->shuffle();
 // 1.a. ensure the deck was shuffled properly:
 print_r($myCards->show_deck());
 
-
 // 1.b. deal cards until they are gone.
-// ( Iterate 54 times to ensure deal_one_card() properly returns false when the cards are gone. )
-for($i=1;$i<=54;$i++){
+// ( Iterate 53 times to ensure deal_one_card() properly returns false when the deck is empty. )
+for($i=1;$i<=53;$i++){
 	$card = $myCards->deal_one_card();
 	if($card) {
 	 	echo "You were dealt " . $card ."<br>";
